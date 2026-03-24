beeea4b181313e357195b329a64a095edf04f590
cedricbetke <cedricbetke@gmail.com>
2026-03-24 18:44:31 +0100

    refactor index.tsx zbd tsconfig.json; delete unsused files

--- apps/client/app/index.tsx
+++ apps/client/app/index.tsx
@@ -1,17 +1,9 @@
-import {Image, StyleSheet, Platform, ScrollView} from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import {useTheme} from "react-native-paper";
-
-import { Text, View, /* @tutinfo Import <CODE>StyleSheet</CODE> to define styles. */ } from 'react-native';
+import { View } from 'react-native';
 import InvTable from "@/src/features/inventory/components/InvTable";
 import TopBar from "@/src/shared/components/navigation/TopBar";
 import { InventoryProvider } from "@/src/features/inventory/context/InventoryContext";
 
 export default function Index() {
-    const theme = useTheme();
     return (
         <InventoryProvider>
             <View style={{height: "100%"}}>
